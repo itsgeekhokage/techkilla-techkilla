dcc815444380263a9a833950b816f268c630bd28
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-03-13 12:17:18 +0530

    image issue

--- src/pages/aboutPage/peoplelovesection.js
+++ src/pages/aboutPage/peoplelovesection.js
@@ -1,36 +1,46 @@
-import tailorblackImg from "../../assets/about/tailoiredconceptualisation.png";
+import tailoredConceptualisationBlackImg from "../../assets/about/tailoiredconceptualisation.png";
+import efficientExecutionBlackImg from "../../assets/about/efficientexecution.png";
+import technologyIntegrationBlackImg from "../../assets/about/technologyintegiabion.png";
+import pleasantConversationsBlackImg from "../../assets/about/pleasantconversation.png";
+import happyFacesBlackImg from "../../assets/about/happyfaces.png";
+
+import icon17ColorImg from "/src/assets/about/icon17.png";
+import icon16ColorImg from "/src/assets/about/icon16.png";
+import icon15ColorImg from "/src/assets/about/icon15.png";
+import icon14ColorImg from "/src/assets/about/icon14.png";
+import icon19ColorImg from "/src/assets/about/icon19.png";
+
 const list = [
     {
         title: "Tailored Conceptualisation",
         description: "",
-        blackimg: tailorblackImg,
-        colorimg: "/src/assets/about/icon17.png"
+        blackimg: tailoredConceptualisationBlackImg,
+        colorimg: icon17ColorImg
     },
     {
         title: "Efficient Execution",
         description: "",
-        blackimg: "/src/assets/about/efficientexecution.png",
-        colorimg: "/src/assets/about/icon16.png"
+        blackimg: efficientExecutionBlackImg,
+        colorimg: icon16ColorImg
     },
     {
-        title: "Technology Integeration",
+        title: "Technology Integration",
         description: "",
-        blackimg: "/src/assets/about/technologyintegiabion.png",
-        colorimg: "/src/assets/about/icon15.png"
+        blackimg: technologyIntegrationBlackImg,
+        colorimg: icon15ColorImg
     },
     {
         title: "Pleasant Conversations",
         description: "",
-        blackimg: "/src/assets/about/pleasantconversation.png",
-        colorimg: "/src/assets/about/icon14.png"
+        blackimg: pleasantConversationsBlackImg,
+        colorimg: icon14ColorImg
     },
     {
         title: "Happy Faces",
         description: "",
-        blackimg: "/src/assets/about/happyfaces.png",
-        colorimg: "/src/assets/about/icon19.png"
+        blackimg: happyFacesBlackImg,
+        colorimg: icon19ColorImg
     },
-
-]
+];
 
 export default list;
