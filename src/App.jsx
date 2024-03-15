6583db95e94833479dcda8be2d397c6128bdc041
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-03-15 11:33:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,30 @@ import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import AboutPage from "./pages/aboutPage/AboutPage";
 import ProjectsPage from "./pages/projectsPage/ProjectsPage";
+import PreLoader from "./components/home/preLoader/PreLoader";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/project" element={<ProjectsPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <>
+      <PreLoader/>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={<HomePage />}
+          />
+          <Route
+            path="/about"
+            element={<AboutPage />}
+          />
+          <Route
+            path="/project"
+            element={<ProjectsPage />}
+          />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
   );
 }
